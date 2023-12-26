fea0ab3a54496199d48ff7a706ee39f098c903d4
DolipraneXD <149811816+DolipraneXD@users.noreply.github.com>
2023-12-26 09:52:24 +0100

    done

--- main.c
+++ main.c
@@ -6,21 +6,18 @@
 /*   By: moel-fat <moel-fat@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 20:11:29 by moel-fat          #+#    #+#             */
-/*   Updated: 2023/12/23 21:35:47 by moel-fat         ###   ########.fr       */
+/*   Updated: 2023/12/25 17:43:38 by moel-fat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include  "get_next_line.h"
+#include "get_next_line.h"
 
-void	f(void)
-{
-	system("leaks a.out");
-}
 int	main(void)
 {
-	// atexit(f);
-	int fd = open("test.txt",  O_RDONLY);
-	char *s;
+	int		fd;
+	char	*s;
+
+	fd = open("test.txt");
 	s = get_next_line(fd);
 	while (s)
 	{
